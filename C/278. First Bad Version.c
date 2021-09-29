1f39208fd537bd5403d7806aa2af2645eadd2ded
JamesChen0422 <bondzxcv1308@gmail.com>
2021-09-29 15:10:43 +0800

    Update 278. First Bad Version.c
    add comment to 278

--- C/278. First Bad Version.c
+++ C/278. First Bad Version.c
@@ -5,7 +5,7 @@ int firstBadVersion(int n) {
     int start = 1;
     int end = n;
     while (start < end) {
-        int mid = start + (end - start) / 2;
+        int mid = start + (end - start) / 2;  //prevent overflow
         if (isBadVersion(mid)) {
             end = mid;
         } else {
