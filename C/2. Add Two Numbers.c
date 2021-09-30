91fee9efc6af943e45ce39db8cd1c94191b6e2d1
James Chen <bondzxcv1308@gmail.com>
2021-09-30 09:39:40 +0800

    modified 2. code

--- C/2. Add Two Numbers.c
+++ C/2. Add Two Numbers.c
@@ -1,11 +1,11 @@
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    struct ListNode* ans;
-    ans->val = 0; ans->next = NULL;
-    struct ListNode* l3 = ans;
-    int carry = 0, sum;
+    struct ListNode ans;
+    ans.val = 0; ans.next = NULL;
+    struct ListNode* l3 = &ans;
+    int remainder = 0, sum;
     while (l1 != NULL || l2 != NULL || remainder != 0) {
-        sum = carry + (l1 == 0 ? 0 : l1->val) + (l2 == 0 ? 0: l2->val);
-        carry = sum/10;
+        sum = remainder + (l1 == 0 ? 0 : l1->val) + (l2 == 0 ? 0: l2->val);
+        remainder = sum/10;
         sum %= 10;
         l3->next = malloc(sizeof(struct ListNode));
         l3->next->next = NULL;
@@ -14,8 +14,9 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
         l1 = (l1 == 0 ? 0 : l1->next);
         l2 = (l2 == 0 ? 0 : l2->next);
     }
-    return ans;
+    return ans.next;
 }
+
 
 =====================================================
 
