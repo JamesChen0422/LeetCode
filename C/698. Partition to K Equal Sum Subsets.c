fff6190340b51e57a6d07f8a3d81049221d9ffc5
James Chen <bondzxcv1308@gmail.com>
2021-09-30 17:53:29 +0800

    update 698. status

--- C/698. Partition to K Equal Sum Subsets.c
+++ C/698. Partition to K Equal Sum Subsets.c
@@ -1,6 +1,8 @@
+not finish
+
 
 bool canPartitionKSubsets(int* nums, int numsSize, int k){
-    int sum=0, max, group;
+    int sum=0, max, group, temp;
     for(int i=0; i<numsSize; i++){
         sum+=nums[i];
     }
@@ -9,12 +11,20 @@ bool canPartitionKSubsets(int* nums, int numsSize, int k){
         return false;
     
     max=(sum/k)+1;
-    group=numsSize/k;
+    group=(numsSize/k)+(numsSize%k);
     
+    for(int i=0; i<numsSize; i++){
+        if(nums[i+1]<nums[i]){
+            temp=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
+        }
+    }
+
     for(int i=0; i<numsSize; i++){
         if(nums[i]>max)
             return false;
-        
+    
     }
     
     return true;
