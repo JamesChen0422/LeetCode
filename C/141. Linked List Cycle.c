fe60a0c2ae1bf674cbff66641c7e48c1f9c68501
JamesChen0422 <bondzxcv1308@gmail.com>
2021-10-03 15:54:06 +0800

    Add 141. C solution into C folder

---
+++ C/141. Linked List Cycle.c
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+bool hasCycle(struct ListNode *head) {
+    struct ListNode * temp=head,*temp2=NULL;
+    
+    if(!head || !(head->next))
+        return false;
+    
+    temp2=head->next;
+    
+    while(temp!=temp2)
+    {
+        if(temp2==NULL)
+        {
+            return false;
+        }
+        temp=temp->next;
+        if(temp2->next && temp2->next->next)
+            temp2=temp2->next->next;
+        else
+            temp2 = temp2->next;
+    }
+    
+    return true;
+}
