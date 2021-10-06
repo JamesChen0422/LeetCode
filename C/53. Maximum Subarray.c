a65155494f5a8dadecacc43e4110d65c9cca5e4e
James Chen <bondzxcv1308@gmail.com>
2021-10-06 14:15:55 +0800

    Modify new solution of 53.

---
+++ C/53. Maximum Subarray.c
@@ -0,0 +1,12 @@
+
+
+int maxSubArray(int* nums, int numsSize){
+    int sub=numsSize, sum=0, temp=0;
+    
+    while(sub>0){
+        for(int i=0; i<i+numsSize-sub; i++)
+         
+    }
+    
+    return sum;
+}
